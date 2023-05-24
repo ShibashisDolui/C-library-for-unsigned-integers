00165ceee34eb76c6df525413c6b3192859f8061
lui215 <shibashisdolui99@gmail.com>
2023-05-24 17:35:16 +0530

    Created a library for big integers in C++

---
+++ sd.cpp
@@ -0,0 +1,9 @@
+#include "myBigIntegers.h"
+#include<iostream>
+using namespace std;
+int main()
+{
+    Integer i("1345636346352"), j("22342353");
+    cout<<factorial(Integer("5"))<<endl;
+    cout<<i+j;
+}
